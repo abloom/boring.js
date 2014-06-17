e519ebdc2a510fcdeab4b3c9b226cc26adf743c2
Andrew Bloom <andrew@andrewkbloom.com>
2014-06-16 23:33:33 -0500

    cleanup

--- nodes/omxplayer.js
+++ nodes/omxplayer.js
@@ -2,23 +2,18 @@ var exec = require('child_process').exec;
 
 module.exports = function(RED) {
   function OmxPlayer(n) {
-      RED.nodes.createNode(this,n);
+    RED.nodes.createNode(this,n);
 
-      var node = this;
-      this.on("input", function(msg) {
-          if (msg != null) {
-              var cl = node.cmd+" "+msg.payload+" "+node.append;
-              node.log(cl);
-              var child = exec(cl, function (error, stdout, stderr) {
-                  msg.payload = stdout;
-                  var msg2 = {payload:stderr};
-                  if (error !== null) {
-                      var msg3 = {payload:error};
-                  }
-                  node.send([msg,msg2,msg3]);
-              });
-          }
-      });
+    var node = this;
+    this.on("input", function(msg) {
+      if (msg != null) {
+        var cl = "/usr/bin/omxplayer "+msg.payload;
+        node.log(cl);
+        var child = exec(cl, function (error, stdout, stderr) {
+            node.send({payload: stdout});
+        });
+      }
+    });
   }
 
   RED.nodes.registerType("omxplayer", OmxPlayer);
