815bfa35b17f5d8390386c34efc06458df6fdfcc
Andrew Bloom <andrew@andrewkbloom.com>
2014-06-18 00:12:24 -0500

    kill omxplayer by sending a q

--- nodes/omxplayer.js
+++ nodes/omxplayer.js
@@ -10,7 +10,7 @@ module.exports = function(RED) {
     this.on("input", function(msg) {
       if (!msg) { return; }
 
-      if (child) { child.kill('SIGINT'); }
+      if (child) { child.stdin.write("q"); }
 
       var cl = "/usr/bin/omxplayer " + msg.payload;
       node.log(cl);
